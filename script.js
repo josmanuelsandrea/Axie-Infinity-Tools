571e7e3e25f30fa5173a9377937cc8c0fd87d877
Josmanuel <josmanueljsg@gmail.com>
2021-10-26 22:59:27 -0500

    AIC | 0.4 | new Actions Registry

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function addToHistory(td1, td2, td3, td4, td5){
+    let table = document.querySelector('.container__history table tbody');
+
+    let data = `<tr class="history__round">
+        <td>${td1}</td>
+        <td>${td2} <i class="fas fa-bolt"></i></td>
+        <td>${td3} <i class="fas fa-plus"></i></td>
+        <td>-${td4}</td>
+        <td>${td5} <i class="fas fa-ban"></i></td>
+    </tr>`
+
+    table.innerHTML += data;
+}
+
+
+
 function Earn(){
     let label = document.querySelector('.earn__label__number');
     let value = parseInt(label.innerText);
@@ -26,6 +42,14 @@ function Done(){
     let actualEnergy = document.querySelector('.counter__number');
     let roundCounter = document.querySelector('.roundCounter__number');
 
+    addToHistory(
+        roundCounter.innerText,
+        actualEnergy.innerText,
+        earnN,
+        spendN,
+        stoleN
+    )
+
     actualEnergy.innerText = ((parseInt(actualEnergy.innerText) - spendN) - stoleN) + earnN + 2;
 
     if (parseInt(actualEnergy.innerText) < 0) actualEnergy.innerText = 2;
@@ -38,6 +62,8 @@ function Done(){
     document.querySelector('.stole__label__number').innerText = 0;
     document.querySelector('.stole__label__sign').innerText = '';
     roundCounter.innerText = parseInt(roundCounter.innerText) + 1; 
+
+
 }
 
 function Substract(type){
@@ -47,7 +73,22 @@ function Substract(type){
 }
 
 function Reset(){
-    location.reload();
+    let actualEnergy = document.querySelector('.counter__number');
+    let roundCounter = document.querySelector('.roundCounter__number');
+
+    roundCounter.innerText = 1; 
+    actualEnergy.innerText = 3;
+
+    document.querySelector('.earn__label__number').innerText = 0;
+    document.querySelector('.earn__label__sign').innerText = '';
+    document.querySelector('.spend__label__number').innerText = 0;
+    document.querySelector('.spend__label__sign').innerText = '';
+    document.querySelector('.stole__label__number').innerText = 0;
+    document.querySelector('.stole__label__sign').innerText = '';
+
+    document.querySelectorAll('.history__round').forEach(item=>{
+        item.remove();
+    });
 }
 
 let checkbox = document.getElementById('activateCheckbox');
